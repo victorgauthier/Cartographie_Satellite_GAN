9e92ea6985a17570692b39f47298f9612105f01f
victorgauthier <51914546+victorgauthier@users.noreply.github.com>
2022-04-09 16:20:02 +0200

    Add loss to assets

--- hyper_parameters.py
+++ hyper_parameters.py
@@ -6,7 +6,7 @@ BETA1 = 0.5 # Suggested by the original paper of pix2pix
 BETA2 = 0.999 # Suggested by the original paper of pix2pix
 NUM_EPOCHS = 200 
 NGPU = 1
-L1_LAMBDA = 100  # <= 0 ---> Loss not calculated
+L1_LAMBDA = 0  # <= 0 ---> Loss not calculated
 L2_LAMBDA = 0  # <= 0 ---> Loss not calculated
 P_LAMBDA = 0 # <= 0 ---> Loss not calculated
 
